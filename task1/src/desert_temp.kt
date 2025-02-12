867bfc9c396679a3408d89030dbe78be6e5ef3c3
Sarah <sarahalenezi96@outlook.com>
2025-02-12 18:19:57 +0300

    edited code

--- task1/src/desert_temp.kt
+++ task1/src/desert_temp.kt
@@ -1,10 +1,12 @@
+
 fun main()
 {
-    println(currentTemperature(25,40))
+    println("Average Temperature: " + currentTemperature(25,40))
 }
+fun currentTemperature(morningTemperature:Int,afternoonTemperature:Int): Double {
+    var avg:Double = ((morningTemperature.toDouble() + afternoonTemperature.toDouble()) / 2)
+    return avg
 
-fun currentTemperature(morningTemperature:Int,afternoonTemperature:Int): String {
-    return "Average Temperature: " + ((morningTemperature.toDouble() + afternoonTemperature.toDouble()) / 2)
     //I casted the integers because average values are usually Double
     // And in this case, the real answer is 32.5 for the given temp values
 }
